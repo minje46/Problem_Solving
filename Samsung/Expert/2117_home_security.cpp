3e993385b46c96b74a01ca26d69dcaa5b11403be
minje46 <minjea1701@naver.com>
2019-08-31 17:24:16 +0900

    Expert_home_security / [Simulation]
    To figure out the maximum number of home for home security based on simulation problem.
    Explaination is attached as comment(annotation).

--- Samsung/Expert/2117_home_security.cpp
+++ Samsung/Expert/2117_home_security.cpp
@@ -4,88 +4,110 @@
 #include<cmath>
 //#pragma warning(disable:4996)
 
-#define MAX 21
+#define MAX 22
 
 using namespace std;
 
-int N, M;		// N = The size of map.		M = The cost to pay from each home.
+int N, M, K;					// N = The size of map.		M = The cost to pay from each home.
 int map[MAX][MAX];		// map = The memory of original map data.
+bool visit[MAX][MAX];		// visit = The memory of safe area or not.
+int cost[MAX];				// cost = The memory of safe security's cost.
+int answer = 0;				// answer = The maximum number of home as output.
 
-bool Range(int y, int x)		// To check whether (y,x) is possible area or not.
+int Count()		// To count the number of home which is located in safe security.
 {
-	if ((y >= 0 && y < N) && (x >= 0 && x < N))		// Possible range.
-		return true;
-	else
-		return false;
-}
-
-int Count(int y, int x, int k)		// To count the number of house.
-{
-	int cnt = 0, nx, endp;		
-	for (int i = 1; i < k; i++)
+	int cnt = 0;
+	for (int i = 0; i < N; i++)
 	{
-		int up = y - k + i;
-		int down = y + k - i;
-
-		endp = i * 2 - 1;
-		nx = x - endp / 2;
-
-		for (int j = 0; j < endp; j++)
+		for (int j = 0; j < N; j++)
 		{
-			if (Range(up, nx + j) && (map[up][nx + j]))
-				cnt++;
-			if (Range(down, nx + j) && (map[down][nx + j]))
-				cnt++;
+			if (visit[i][j] && map[i][j] == 1)		// [visit = true] is safe area.
+				cnt++;										// [map = 1] is home.
 		}
-	}
-
-	endp = k * 2 - 1;
-	nx = x - endp / 2;
-	for (int i = 0; i < endp; i++)
-	{
-		if (Range(y, nx + i) && map[y][nx + i])
-			cnt++;
 	}
 	return cnt;
 }
 
+void Safe_area(int y, int x, int k)		// To check the range of safe security.
+{
+	for (int i = 0; i < k; i++)			// Middle range.			
+	{
+		if (x - i >= 0)					// Not overflow.
+			visit[y][x - i] = true;
+		if (x + i < N)
+			visit[y][x + i] = true;
+	}
+
+	for (int i = 1; i < k; i++)			// Up and Down range.
+	{
+		for (int j = 0; j < k - i; j++)		// Middle range.
+		{
+			if (y - i >= 0)		// Upper.
+			{
+				if (x - j >= 0)	// Left.
+					visit[y - i][x - j] = true;
+				if (x + j < N)	// Right.
+					visit[y - i][x + j] = true;
+			}
+			if (y + i < N)		// Down.
+			{
+				if (x - j >= 0)	// Left.
+					visit[y + i][x - j] = true;
+				if (x + j < N)	// Right.
+					visit[y + i][x + j] = true;
+			}
+		}
+	}
+}
+
+void Simulation(int y, int x)		// To figure out the safe secrutiy from the current location.
+{
+	for (int i = 1; i < MAX; i++)	// Check the maximum range.
+	{										// [큰 범위에서 만족하는 범위가 존재할 수 있다.]
+		memset(visit, false, sizeof(visit));		// Initialization.
+		Safe_area(y, x, i);			// Safe area.
+		int home = Count();			// The number of home.
+
+		if (home * M >= cost[i])	// Base case.
+			answer = max(answer, home);		// Compare the number of home.
+	}
+}
 
 int main(void)
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	//freopen("input.txt", "r", stdin);
+//	freopen("input.txt", "r", stdin);
 
 	int test_case;
 	cin >> test_case;
 	for (int t = 1; t <= test_case; t++)
 	{	// Initialization.
 		memset(map, 0, sizeof(map));
+		int cnt = 0;
 
 		cin >> N >> M;
 		for (int i = 0; i < N; i++)
 		{
 			for (int j = 0; j < N; j++)
-				cin >> map[i][j];
+			{
+				cin >> map[i][j];		// Input.
+				if (map[i][j] == 1)		// Home.
+					cnt++;					
+			}
 		}
 
-		int answer = 0;		// answer = The maximum number of homes to be included in security area.
+		for (int i = 1; i < MAX; i++)	// Cost of safe security.
+			cost[i] = pow(i, 2) + pow(i - 1, 2);
+
+		answer = 0;
 		for (int i = 0; i < N; i++)
 		{
 			for (int j = 0; j < N; j++)
-			{
-				for (int k = 1; k <= MAX; k++)
-				{
-					int cnt = Count(i, j, k);
-					int service = pow(k, 2) + pow(k - 1, 2);
-					int cost = cnt * M;
-
-					if (cost >= service)
-						answer = max(answer, cnt);
-				}
-			}
+				Simulation(i, j);			// Simulation.
 		}
-		cout << "#" << t << " " << answer << endl;
+
+		cout << "#" << t << " " << answer << endl;		// Output.
 	}
 }
