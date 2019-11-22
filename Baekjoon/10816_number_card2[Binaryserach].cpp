36de78fd7c716e37a87a2f8c1bbc6ff769975b19
minje46 <minjea1701@naver.com>
2019-11-22 13:57:30 +0900

    acmicpc_number_card2 / [Binary search]
    To figure out the number card comparison based on binary search.
    Explaination is attached as comment(annotation).

--- Baekjoon/10816_number_card2[Binaryserach].cpp
+++ Baekjoon/10816_number_card2[Binaryserach].cpp
@@ -4,18 +4,27 @@
 
 using namespace std;
 
-int N, M;				// N = The number of cards as input.					M = The number of cards possessed.
-	// card = The memory of input data.
-map<int, int> mp;
-vector<pair<int, int>>vc;
+int N, M;							// N = The number of cards as input.					M = The number of cards possessed.
+map<int, int> mp;			// mp = The memory of input data. [Card, Count]
+vector<pair<int, int>>vc;	// vc = The memory of number cards.
 
-int Binary(int value)
+int Binary(int value)			// To figure out the number exists or not based on binary search.
 {
-	int lf = 0, rt = mp.size();
-	while (lf < rt)
+	int lf = 0, rt = mp.size()-1;	// Left and Right index.
+	while (lf <= rt)
 	{
 		int mid = (lf + rt) / 2;
 
+		if (value == vc[mid].first)		// Base case.
+			return vc[mid].second;	// Find the value.
+
+		if (lf >= rt)			// Base case.
+			return 0;			// Overflow.
+
+		if (vc[mid].first > value)		// Go left.
+			rt = mid;
+		else					// Go right.
+			lf = mid + 1;
 	}
 }
 
@@ -27,21 +36,21 @@ int main(void)
 	cin >> N;
 	for (int i = 0; i < N; i++)
 	{
-		int num;
-		cin >> num;
-		if (mp.find(num) == mp.end())
-			mp.insert(make_pair(num, 1));
-		else
-			mp.find(num)->second += 1;
+		int num;		
+		cin >> num;										// Input.
+		if (mp.find(num) == mp.end())				// New number card.
+			mp.insert(make_pair(num, 1));		
+		else													// Number is already existed.
+			mp.find(num)->second += 1;			// Increase the count.
 	}
-	for (auto it : mp)
-		vc.push_back(make_pair(it.first, it.second));
+	for (auto it : mp)										// Copy the data in vector data structure.
+		vc.push_back(make_pair(it.first, it.second));		// To search easily with index.
 
 	cin >> M;
 	for (int i = 0; i < M; i++)
 	{
 		int num;
 		cin >> num;
-		cout << Binary(num) << " ";
+		cout << Binary(num) << " ";						// Output.	
 	}
 }
